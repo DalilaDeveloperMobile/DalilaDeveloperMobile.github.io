228ee559aeb6407494486a0342e3890eb1e73344
Dalila Custódio <dalila.dalila70@gmail.com>
2026-03-25 16:48:32 -0300

    add
    add

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2221680248"
+    serviceWorkerVersion: "2675673837"
   }
 });
